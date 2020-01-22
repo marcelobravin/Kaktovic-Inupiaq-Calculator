bc18f8c6324cde5e98ecc9a2049e18b7bb17cef7
marcelo.bravin <marcelo.bravin@gmail.com>
2020-01-22 17:02:16 -0300

    Correções

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,40 +7,52 @@ $(document).ready(function(){
         var simb = $(this).text();
         var x = $("#termo").val();
         var expression = x+""+simb; 
-        var dec_termo = ki_to_decimal_expression(expression)
+        var dec_termo = ki_to_decimal_expression(expression);
         $("#termo").val(expression);
         $("#termo").attr({ 'title': dec_termo.toString() });
     });
 
     /* Botao limpar */
-    $("#limpar").click(function(){
+    $("#limpar").click(function()
+    {
         $("#termo, #resposta").val("");
         $("#termo").removeAttr('title');
         $("#resposta").removeAttr('title');
     });
 
     /* Adiciona simbolo da operacao */
-    $(".operacao").click(function(){
+    $(".operacao").click(function()
+    {
         var operacaoSelecionada = $(this).text();
         var x = $("#termo").val();
         $("#termo").val(x+""+  operacaoSelecionada);
     });
 
     /* Calcula */
-    $("#calcular").click(function(){
+    $("#calcular").click(function()
+    {
         $("#resposta").removeAttr('title');
         var ki_termo = $("#termo").val();
-        var dec_termo = ki_to_decimal_expression(ki_termo)
+        var dec_termo = ki_to_decimal_expression(ki_termo);
         
         calcularController ( dec_termo );
     });
-
 });
 
-function calcularController ( stringConta ) 
+
+// ----------------------------------------------
+//  CONTROLLER =
+// ----------------------------------------------
+
+/**
+ * calcularController
+ * @param stringConta
+ * @returns {boolean}
+ */
+function calcularController ( stringConta )
 {
-    var vetorConta = stringConta.split(' ');
-    var keyOperador = posicaoOperador(vetorConta)
+    let vetorConta = stringConta.split(' ');
+    let keyOperador = posicaoOperador(vetorConta);
 
     if(!keyOperador) {
         $("#resposta").attr({ 'title': stringConta.toString() });
@@ -54,20 +66,20 @@ function calcularController ( stringConta )
 
     switch (op) {
         case "*":
-            var resultado = multplicarService(n1, n2)
-        break;
+            var resultado = multplicar(n1, n2);
+            break;
 
         case "/":
-            var resultado = dividirService(n1, n2)
-        break;
+            var resultado = dividir(n1, n2);
+            break;
 
         case "+":
-            var resultado = somaService(n1, n2)
-        break;
+            var resultado = somar(n1, n2);
+            break;
 
         case "-":
-            var resultado = subtrairService(n1, n2)
-        break;
+            var resultado = subtrair(n1, n2);
+            break;
     }
 
     var stringConta2 = ajustaTermo(vetorConta, parseInt(keyOperador), resultado )
@@ -76,7 +88,16 @@ function calcularController ( stringConta )
 
 }
 
-function posicaoOperador ( array ) 
+// ----------------------------------------------
+//  SERVICES =
+// ----------------------------------------------
+
+/**
+ * posicaoOperador
+ * @param array
+ * @returns {*}
+ */
+function posicaoOperador ( array )
 {
     if( jQuery.inArray( "*", array ) > 0 ){
         return array.indexOf("*");
@@ -93,21 +114,28 @@ function posicaoOperador ( array )
     if( jQuery.inArray( "-", array ) > 0  ){
         return array.indexOf("-");
     }
-    
+
     return false;
 }
 
-function ajustaTermo ( vetorConta, keyOperador, resultado ) 
+/**
+ * ajustaTermo
+ * @param vetorConta
+ * @param keyOperador
+ * @param resultado
+ * @returns {string}
+ */
+function ajustaTermo ( vetorConta, keyOperador, resultado )
 {
     var min = keyOperador - 1;
 
     for (let i = 0; i < vetorConta.length; ++i) {
-        
-        if (i == min) {
+
+        if (i === min) {
             vetorConta[min] = resultado.toString()
         }
 
-        if (i == keyOperador) {
+        if (i === keyOperador) {
             vetorConta.splice(i, 2);
         }
     }
@@ -116,27 +144,69 @@ function ajustaTermo ( vetorConta, keyOperador, resultado )
 
 }
 
-function decimal_to_ki_expression ( str ) 
+
+/**
+ * somar
+ * @param n1
+ * @param n2
+ * @returns {number}
+ */
+function somar ( n1, n2 )
 {
-
-    var vetorConta = str.split(' ');
-
-    for (let i = 0; i < vetorConta.length; i++) {
-        var number = array[i].isInteger();
-        vetorConta[i] = ki_encode(number)
-    }
-
-    return  vetorConta.join(' ');
-
+    return parseInt(n1) + parseInt(n2);
 }
 
-function ki_to_decimal_expression( ki_expressao ) {
+/**
+ * subtrair
+ * @param n1
+ * @param n2
+ * @returns {number}
+ */
+function subtrair ( n1, n2 )
+{
+    return parseInt(n1) - parseInt(n2);
+}
+
+/**
+ * multplicar
+ * @param n1
+ * @param n2
+ * @returns {number}
+ */
+function multplicar ( n1, n2 )
+{
+    return parseInt(n1) * parseInt(n2);
+}
+
+/**
+ * dividir
+ * @param n1
+ * @param n2
+ * @returns {number}
+ */
+function dividir ( n1, n2 )
+{
+    return parseInt(n1) / parseInt(n2);
+}
+
+
+// ----------------------------------------------
+//  HELPERS =
+// ----------------------------------------------
+
+/**
+ * ki_to_decimal_expression
+ * @param ki_expressao
+ * @returns {string}
+ */
+function ki_to_decimal_expression( ki_expressao )
+{
 
     var vetorConta = ki_expressao.split(' ');
 
     for (let i = 0; i < vetorConta.length; i++) {
 
-        str = ki_decode(vetorConta[i])
+        let str = ki_decode(vetorConta[i]);
 
         if(is_Int(str)){
              vetorConta[i] = str
@@ -148,41 +218,50 @@ function ki_to_decimal_expression( ki_expressao ) {
     return  vetorConta.join(' ');
 }
 
+/**
+ * validate is_Int
+ * @param num
+ * @returns {boolean}
+ */
 function is_Int(num) 
 {
     if (typeof num !== 'number') {
         return false; 
     }
    
-   return !isNaN(num) && parseInt(Number(num)) == num && !isNaN(parseInt(num, 10));
+   return !isNaN(num) && parseInt(Number(num)) === num && !isNaN(parseInt(num, 10));
  }
 
-function ki_encode ( dec_number ) 
+/**
+ * ki_encode
+ * @param dec_number
+ * @returns {string}
+ */
+function ki_encode ( dec_number )
 {
     var simbulos = {
            0 : "",   1 : "",   2 : "",   3 : "",   4 : "",   5 : "",   6 : "",   7 : "",   8 : "",   9 : ""
         , 10 : "",  11 : "",  12 : "",  13 : "",  14 : "",  15 : "",  16 : "",  17 : "",  18 : "",  19 : ""
-    }
+    };
     
-    // Converter de base 10 para base 20
-    var numeros = []
+    var numeros = [];
     var simbs = [];
 
-    function transformBase10toBase20(dec_num) { // 20
+    function transformBase10toBase20(dec_num) {
         
-        var resultado = parseInt(dec_num / 20)    
-        var sobra     = dec_num % 20
+        var resultado = parseInt(dec_num / 20);
+        var sobra     = dec_num % 20;
     
         numeros.push(sobra);
         
         if (resultado <= 0) {
-            return "false"
+            return true;
         }
         
         transformBase10toBase20(resultado)
     }
 
-    transformBase10toBase20(dec_number)
+    transformBase10toBase20(dec_number);
 
     for (let i = 0; i < numeros.length; i++) {
         simbs[i] = simbulos[numeros[i]]
@@ -191,15 +270,19 @@ function ki_encode ( dec_number )
     return simbs.reverse().join('');
 }
 
-
+/**
+ * ki_decode
+ * @param ki_number
+ * @returns {*}
+ */
 function ki_decode ( ki_number ) 
 {
-    var v_ki = ki_number.split('');
+    let v_ki = ki_number.split('').reverse();
 
-    var numeros = {
+    let numeros = {
           "" :  0, "" :  1, "" :  2, "" :  3, "" :  4, "" :  5, "" :  6, "" :  7, "" :  8, "" :  9
         , "" : 10, "" : 11, "" : 12, "" : 13, "" : 14, "" : 15, "" : 16, "" : 17, "" : 18, "" : 19
-    }
+    };
 
     // Converter de base 10 para base 20
     let p_math = 1;
@@ -209,7 +292,7 @@ function ki_decode ( ki_number )
 
     // monta base de multiplicação
     for (let i = 1; i < v_ki.length; i++) {
-        p_math    = parseInt(p_math * 20)
+        p_math    = parseInt(p_math * 20);
         v_calc[i] = p_math
     }
     
@@ -220,36 +303,5 @@ function ki_decode ( ki_number )
     }
 
     const reducer = (accumulator, currentValue) => accumulator + currentValue;
-    // console.log(v_calc, v_dec, v_prod, v_prod.reduce(reducer))
     return v_prod.reduce(reducer)
-}
-
-function somaService ( n1, n2 ) 
-{
-    return parseInt(n1) + parseInt(n2);
-}
-
-function subtrairService ( n1, n2 ) 
-{
-    return parseInt(n1) - parseInt(n2);
-}
-
-function multplicarService ( n1, n2 ) 
-{
-    return parseInt(n1) * parseInt(n2);
-}
-
-function dividirService ( n1, n2 ) 
-{
-    return parseInt(n1) / parseInt(n2);
-
-    if ( resposta > 19 ) {
-        sobra = resposta % 20;
-        resposta = parseInt(resposta / 20);
-    }
-
-    if (op == '/') {
-        console.log(" divisão "+ resposta + " sobra: "+sobra);
-    }
-    console.log( respostaDecimal );
 }
