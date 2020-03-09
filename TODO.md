444326b7a7f2f1523044e69423428cc6b3e06842
marcelo.bravin <marcelo.bravin@gmail.com>
2020-03-09 11:27:07 -0300

    General fixes and otimizations, full commit

--- TODO.md
+++ TODO.md
@@ -1,19 +1,26 @@
-# Ajustar a ser realizados
+# Ajustes a serem realizados
 
 ## Matemática
-- tratar numeros fracionados
+- tratar numeros fracionários
 - tratar numeros negativos
-- √
+- 3.33 numeros infinitamente longos
+    - retornar aproximadamente ≃
+- calculadora cientifica e financeira
+    - √
+- novas funções . ( ) ^
+- adicionar contas decimais (1.1+1.1)
+- verificar divisão
+    contas que causam resultados incoerentes
+    // 1/1000
 
 ## Documentação
 - arrumar readme (ingles e bem descrito)
 - documentar funções
 - fazer fluxograma
 - meta authors
-- disseminar
+- criar diagrama
 
 ## Estrutura
-- quebrar algumas funções em arquivos (helpers deixar separados)
 - android
 - nomes das letras
 
@@ -22,125 +29,17 @@
 - tableless
 - maxlenght
 - tirar center
+- baixar CDNs
+    https://code.jquery.com/jquery-3.4.1.slim.min.js
+    https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js
+    https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js
 
 # bugs conhecidos
-- ajustar várias formulas (+-* dá problema)
 - zeros a esquerda
 
 ## Organizar
-- novos botões . ( ) ^
-- 3.33 ????
-    - retornar aproximadamente
-- calculadora cientifica e financeira
-- arrumar readme
-- parseInt(text).toString(20) // verificar se fica mais fácil KI_encode e KI_decode
-- unificar vetor de caracteres no ki_encode e ki_decode
-- baixar CDNs
-- criar diagrama
-
-// So calcular se tiver operacao
-
-//funcoes
-    function printResult
-    function calcularController
-    function posicaoOperador
-    function ajustaTermo
-    function add
-    function subtract
-    function multiply
-    function division
-    function ki_to_decimal_expression
-    function is_Int
-    function isNegative
-    function ki_encode
-    function integer_Base10toBase20
-    function float_Base10toBase20
-    function ki_decode
-
-// eventos
-    /* Coloca os numeros e simbolos no palco */
-    Clicar em calculadora
-        $("#calculadora .numeral").click
-    /* Botao limpar */
-    Clicar em limpar
-        $("#limpar").click
-    /* Adiciona simbolo da operacao */
-    Clicar em operacao
-        $(".operacao").click
-    /* Calcula */
-    Clicar em calcular
-        $("#calcular").click
-
-
-ALGORITMO
-
-
-
-recebe valor
-calcula valor das casas
-recebe operação
-calcula v1 op v2
-converte dec -> KI
-exibe KI e dec
-
-
-
-bug: não permite começar com número negativo
-popover no resultado
-
-
-contas que causam resultados incoerentes
-// 1/1000
-
-
-
-
-
-
-
-
-
-
-atualizar valor termo
-    recebe expressao
-    quebra expressao por operacao
-    pega ultimo termo
-    converte ultimo termo de KI para decimal
-    substitui ultimo termo
-    concatena termos
-    retorna
-
-
-
-
-
-<script>
-    var expressao = "421+111";
-
-    corrigeUltimoTermo(expressao);
-
-
-
-    function converte(pa) {
-        pa = parseInt(pa, 20);
-        return pa.toString(10);
-    }
-
-    function quebra(expressao) {
-        return expressao.split("+");
-    }
-
-    function corrigeUltimoTermo(expressao) {
-        var vetor = quebra(expressao);
-
-        var valor = converte( vetor[1] );
-
-        var novoValor = converte(valor);
-        vetor[1] = novoValor;
-
-        return vetor;
-}
-</script>
-
-
-
+- disseminar
+- substituir operaçao ao clicar em operações sequenciais
+- popover no resultado
+- código em ingles
+- internacionalização
--- assets/js/script.js
+++ assets/js/script.js
@@ -19,18 +19,20 @@ $(document).ready(function(){
 
         $("#termo").val(ki_expression);
         $("#termo").attr( 'title', dec_expression );
+        preparaCampo();
     });
 
-    // Add simbolo da operacao
+    // Add operation symbol
     $(".operacao").click(function()
     {
         var operacaoSelecionada = $(this).text();
-        let lastChar = $("#termo").attr("title").substr(-2,1);
+        let lastChar = $("#termo").attr("title").substr(-1,1);
 
-        if (lastChar == "" && operacaoSelecionada!='-') { // The first char may be a minus
+        // The first char may be a minus symbol
+        if (lastChar == "" && operacaoSelecionada!='-') {
             return false;
         } else {
-
+            // It blocks adding consecutive operation symbols
             if (lastChar == "+" || lastChar == "-" || lastChar == "×" || lastChar == "÷") { // blocks consecutive operators
                 return false;
             }
@@ -40,18 +42,18 @@ $(document).ready(function(){
         $("#termo").attr( 'title',  $("#termo").attr( 'title')+operacaoSelecionada );
     });
 
-    // Executa calculo
+    // Perform calculation
     $("#calcular").click(function()
     {
         var dec_termo = $("#termo").attr('title');
         if ( dec_termo == "" ) {
             return false;
         }
-        preparaCampo("");
-        calcularController();
+        var decimal_expression = $("#termo").attr('title');
+        calcularController( decimal_expression );
     });
 
-    /* Botao limpar */
+    /* Button clear */
     $("#limpar").click(function()
     {
         $("#termo, #resposta").val("");
@@ -59,52 +61,57 @@ $(document).ready(function(){
     });
 });
 
-function preparaCampo() {
-    let valor = $("#termo").val();
-    var valores = valor.split('+'); //////////////////////////////////////
-
-    var ultimoValor = valores[valores.length-1];
-
-    var x = $("#termo").attr('title');
-    console.log(x);
-    comeco = x.substr(0, x.indexOf("+") );
-    console.log(x);
-    // x = "+";
-
-    fim = x.substr(x.indexOf("+"));
-    // alert("comeco "+ comeco);
-    // alert("fim "+ fim);
-
-    var corrigeUltimoTituloDecimal = corrigeUltimoTitulo2(ultimoValor);
-
-    $("#termo").attr('title', comeco +"+" +corrigeUltimoTituloDecimal);
-}
-
-
-function novoAtualizar() {
-    let valor = $("#termo").val();
-    var valores = valor.split('+'); //////////////////////////////////////
-
-    var ultimoValor = valores[valores.length-1];
-    var corrigeUltimoTituloDecimal = corrigeUltimoTitulo2(ultimoValor);
-
-    let titulo = $("#termo").attr('title');
-    var titulos = titulo.split('+'); //////////////////////////////////////
-
-console.log(titulos);
-console.log(corrigeUltimoTituloDecimal);
-
-}
-
-function corrigeUltimoTitulo2(ki_expression)
+function preparaCampo()
 {
-    let v_term = ki_expression.toString().split('');
+    var titulo = $("#termo").attr('title');
+    var lastOperatorTitle = ultimoOperador(titulo);
+    var tituloInicial = titulo.substring(0,lastOperatorTitle+1);
+
+    let valor = $("#termo").val();
+    var lastOperatorValue = ultimoOperador(valor);
+    var ultimoValorVigesimal = valor.substring(lastOperatorValue+1, valor.length);
+
+    var corrigeUltimoTituloDecimal = corrigeTitulo(ultimoValorVigesimal);
+    $("#termo").attr('title', tituloInicial +""+ corrigeUltimoTituloDecimal);
+}
+
+/**
+ * Ordena posicao dos operadores
+ * @param array
+ * @returns index
+ */
+function ultimoOperador( str )
+{
+    if ( typeof str != undefined) {
+        var v_indexes = [];
+        if ( str.lastIndexOf("×") > 0 )
+            v_indexes.push( str.lastIndexOf("×") );
+
+        if ( str.lastIndexOf("÷") > 0 )
+            v_indexes.push( str.lastIndexOf("÷") );
+
+        if ( str.lastIndexOf("+") > 0 )
+            v_indexes.push( str.lastIndexOf("+") );
+
+        if ( str.lastIndexOf("-") > 0 )
+            v_indexes.push( str.lastIndexOf("-") );
+
+        var biggestNumber = Math.max.apply(Math, v_indexes);
+        return biggestNumber;
+    }
+    
+    return false;
+}
+
+function corrigeTitulo(vigesimalNumber)
+{
+    let v_term = vigesimalNumber.toString().split('');
     var concateno = "";
     for (let i=0; i<v_term.length; i++) {
         concateno += ki_decode(v_term[i]);
     }
     
-    var tituloDecimal = toDecimal2( concateno );
+    var tituloDecimal = toDecimal( concateno );
     return tituloDecimal.toString();
 }
 
@@ -112,11 +119,11 @@ function corrigeUltimoTitulo2(ki_expression)
  * Print Result
  * @param dec_number
  */
-function printResult2(dec_number)
+function printResult(dec_number)
 {
-    var x = ki_encode( dec_number );
+    var num = ki_encode( dec_number );
     var sinal = isNegative(dec_number); 
-    $("#resposta").val( sinal +""+ x );
+    $("#resposta").val( sinal +""+ num );
     $("#resposta").attr( 'title', dec_number.toString() );
 }
 
@@ -124,176 +131,10 @@ function printResult2(dec_number)
 //  CONTROLLER =
 // ----------------------------------------------
 
-function calcularController(decimal_expression) {
-    var decimal_expression = $("#termo").attr('title');
+function calcularController(decimal_expression)
+{
     var newString = decimal_expression.replace(/÷/g, '/'); // corrects symbol
-    newString = decimal_expression.replace(/×/g, '*'); // corrects symbol
+    newString     = newString.replace(/×/g, '*'); // corrects symbol
     var decimal_result = eval(newString);
-    printResult2(decimal_result);
+    printResult(decimal_result);
 }
-
-// ----------------------------------------------
-//  SERVICES =
-// ----------------------------------------------
-
-/**
- * Ordena posicao dos operadores
- * @param array
- * @returns index
- */
-function posicaoOperador ( array )
-{
-    if ( jQuery.inArray( "×", array ) > 0 ) {
-        return array.indexOf("×");
-    }
-
-    if ( jQuery.inArray( "÷", array ) > 0 ) {
-        return array.indexOf("÷");
-    }
-
-    if ( jQuery.inArray( "+", array ) > 0 ) {
-        return array.indexOf("+");
-    }
-
-    if ( jQuery.inArray( "-", array ) > 0 ) {
-        return array.indexOf("-");
-    }
-
-    return false;
-}
-
-/**
- * add
- * @param n1
- * @param n2
- * @returns {number}
- */
-function add ( n1, n2 )
-{
-    return parseInt(n1) + parseInt(n2);
-}
-
-/**
- * subtract
- * @param n1
- * @param n2
- * @returns {number}
- */
-function subtract ( n1, n2 )
-{
-    return parseInt(n1) - parseInt(n2);
-}
-
-/**
- * multiply
- * @param n1
- * @param n2
- * @returns {number}
- */
-function multiply ( n1, n2 )
-{
-    return parseInt(n1) * parseInt(n2);
-}
-
-/**
- * division
- * @param n1
- * @param n2
- * @returns {number}
- */
-function division ( n1, n2 )
-{
-    return parseInt(n1) / parseInt(n2);
-}
-
-// ----------------------------------------------
-//  HELPERS =
-// ----------------------------------------------
-
-/**
- * validate is_Int
- * @param num
- * @returns {boolean}
- */
-function is_Int(num) 
-{
-    num = parseInt(num);
-    if (typeof num !== 'number') {
-        console.log(num + "typeof num !== 'number'");
-        return false;
-    }
-
-    return !isNaN(num) && parseInt(Number(num)) === num && !isNaN(parseInt(num, 10));
- }
-
-/**
- * is Negative
- * @param st_term
- * @returns {string}
- */
-function isNegative(st_term)
-{
-    if ( parseFloat(st_term) < 0 ) {
-        return "-";
-    }
-    return "";
-}
-
-function toDecimal(vigesimal) {
-    return parseFloat(vigesimal, 20).toString(10);
-}
-
-function toDecimal2(decimal) {    
-    var vigesimal = decimal.toString(20);
-    return parseInt(vigesimal, 20);
-}
-
-function toVigesimal(decimal) {
-    return decimal.toString(20).toString(10);
-}
-
-function getKeyByValue(object, value) { 
-    return Object.keys(object).find(
-        key => object[key] === value
-    );
-}
-
-function ki_encode(dec_number) {
-    var vig_number = toVigesimal( Math.abs(dec_number) );
-
-    // In case the vigesimal result have multiple characters
-    var array = vig_number.split('');
-    var x = '';
-    for (let i=0; i<array.length; i++) {
-        x += simbolos[ array[i] ];
-    }
-
-    return x;
-}
-
-function ki_decode(simbolo) {
-    return getKeyByValue(simbolos, simbolo)
-}
-
-const simbolos = {
-       0 : ""
-    ,  1 : ""
-    ,  2 : ""
-    ,  3 : ""
-    ,  4 : ""
-    ,  5 : ""
-    ,  6 : ""
-    ,  7 : ""
-    ,  8 : ""
-    ,  9 : ""
-    , 'a': ""
-    , 'b': ""
-    , 'c': ""
-    , 'd': ""
-    , 'e': ""
-    , 'f': ""
-    , 'g': ""
-    , 'h': ""
-    , 'i': ""
-    , 'j': ""
-};
